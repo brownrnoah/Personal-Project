e12d53bac151b4de3a42b6d28f10ca299af9f462
Noah Brown <brownrnoah@gmail.com>
2018-03-23 01:15:26 -0600

    fixing google map key

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
                     <div className="google_map">
                         <MyMapComponent 
                         isMarkerShown 
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?AIzaSyCN0l4xKsbp-wJJUOWiXbLOSwmvFVaFdYQv=3.exp&libraries=geometry,drawing,places"
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `100%` }} />}
                         mapElement={<div style={{ height: `100%` }} />}/>
